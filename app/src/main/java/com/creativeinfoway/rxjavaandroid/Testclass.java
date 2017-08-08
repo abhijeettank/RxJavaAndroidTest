441bcd68d0d608bb220a3a8894fe0cedc6887976
Abhi <abhijeet.tank@gmail.com>
2017-08-08 17:02:12 +0530

    Last Commit

--- app/src/main/java/com/creativeinfoway/rxjavaandroid/Testclass.java
+++ app/src/main/java/com/creativeinfoway/rxjavaandroid/Testclass.java
@@ -1,5 +1,7 @@
 package com.creativeinfoway.rxjavaandroid;
 
+import android.util.Log;
+
 /**
  * Created by Nitin on 08/08/17.
  */
@@ -8,6 +10,6 @@ public class Testclass {
     //this is test class
     Testclass()
     {
-
+Log.d("aa","aa");
     }
 }
